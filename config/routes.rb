d7dd9224a6ebe2bdf447099175667c52c71c68df
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-13 19:04:58 +0900

    テストコード編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :comments, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :comments
   end
-     resources :users, only: [:show]  
-  end
+  resources :users, only: [:show]
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,16 +2,21 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { 'password123' } # シンプルでバリデーションを通るパスワード
+    password { 'password123' }
     password_confirmation { password }
     family_name { Faker::Name.last_name }
     first_name { Faker::Name.first_name }
-    family_name_kana { Faker::Name.last_name } # 注意: 簡単な例として英語のカナですが、実際のアプリの要件に合わせてください
-    first_name_kana { Faker::Name.first_name } # 同上
+    family_name_kana { 'タナカ' } # 実際のアプリ要件に適合するカナ
+    first_name_kana { 'タロウ' } # 実際のアプリ要件に適合するカナ
     birthdate { Faker::Date.birthday(min_age: 18, max_age: 65) }
 
     after(:build) do |user|
-      user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      file_path = Rails.root.join('public/images/test_image.png')
+      if File.exist?(file_path)
+        user.image.attach(io: File.open(file_path), filename: 'test_image.png')
+      else
+        puts "注意: #{file_path} が存在しません。正しいファイルパスを確認してください。"
+      end
     end
   end
 end
