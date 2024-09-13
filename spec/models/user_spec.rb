d7baaf6587043f6ac2f9e6193b65bd246f521f88
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-13 21:46:53 +0900

    spec/models/user_spec.rb編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,16 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it 'nameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
+
     context '新規登録できない場合' do
-      it 'nameが空では登録できない' do
+      it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
@@ -28,26 +30,31 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
+
       it 'family_nameが空では登録できない' do
         @user.family_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family_name can't be blank")
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
+
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First_name can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
+
       it 'family_name_kanaが空では登録できない' do
         @user.family_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family_name_kana can't be blank")
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
       end
+
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First_name_kana can't be blank")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
+
       it 'birthdateが空では登録できない' do
         @user.birthdate = ''
         @user.valid?
