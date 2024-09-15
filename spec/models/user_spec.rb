f2e5ecd413f568266a1092fd27d9bdcc3b6a27e6
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-15 20:18:37 +0900

    spec/models/user_spec.rbファイル編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,8 +51,23 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
-      it 'passwordが半角英数字混合でない場合は登録できない' do
+      it '英字のみのパスワードでは登録できない' do
         @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'ａｂｃｄｅｆ123' # 全角文字を含むパスワード
+        @user.password_confirmation = 'ａｂｃｄｅｆ123'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
