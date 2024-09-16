e80742a3edaa7ad2f849eca3804d7f2c427a4dab
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-16 19:29:16 +0900

    Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,14 +11,20 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
+    end
+    def show
+      @comment = Comment.new
+      @comments = @item.items.includes(:user)
+      @user = @item.user
+      @items = @user.prototypes
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
-                                 :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :responsibility_id, :region_id,
+                                 :time_id, :price).merge(user_id: current_user.id)
   end
 end
