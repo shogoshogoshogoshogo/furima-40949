b292a91877977a3a34cb88642f7276fccdd01f12
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-22 19:37:51 +0900

    routes.rb一部削除

---
+++ app/javascript/item.js
@@ -0,0 +1,44 @@
+const priceInput = document.getElementById("item-price");
+
+// 商品価格の入力イベントにリスナーを追加
+priceInput.addEventListener("input", () => {
+  const inputValue = parseFloat(priceInput.value);  // 数値に変換
+  console.log(inputValue);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  if (!isNaN(inputValue)) {
+    const taxAmount = Math.floor(inputValue * 0.1);  // 販売手数料を計算
+    const profit = Math.floor(inputValue - taxAmount);  // 販売利益を計算
+
+    // 販売手数料と販売利益を表示
+    addTaxDom.innerHTML = taxAmount.toLocaleString();
+    profitDom.innerHTML = profit.toLocaleString();
+  } else {
+    addTaxDom.innerHTML = '';
+    profitDom.innerHTML = '';
+  }
+});
+
+// turbo:loadとturbo:render用の関数を定義
+function updateTaxAndProfit() {
+  const price = parseFloat(document.getElementById("item-price").value);  // 数値に変換
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  if (!isNaN(price)) {
+    const taxAmount = Math.floor(price * 0.1);  // 販売手数料を計算
+    const profit = Math.floor(price - taxAmount);  // 販売利益を計算
+
+    // 販売手数料と販売利益を表示
+    addTaxDom.innerHTML = taxAmount.toLocaleString();
+    profitDom.innerHTML = profit.toLocaleString();
+  } else {
+    addTaxDom.innerHTML = '';
+    profitDom.innerHTML = '';
+  }
+}
+
+window.addEventListener("turbo:load", updateTaxAndProfit);
+window.addEventListener("turbo:render", updateTaxAndProfit);
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:show]
 end
