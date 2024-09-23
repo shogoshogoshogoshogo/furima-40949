5584ee6fc34f5935559e80c8baba148f9669660d
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-23 16:08:52 +0900

    factorybot編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Commerce.product_name }
-    description { Faker::Lorem.sentence }
-    category_id { rand(1..10) } # ランダムなカテゴリIDを設定
-    status_id { rand(1..5) }    # ランダムなステータスIDを設定
-    responsibility_id { rand(1..5) } # 適切なランダムな値を設定
-    region_id { rand(1..47) }   # ランダムな地域IDを設定（47都道府県を想定）
-    time_id { rand(1..5) }      # ランダムな時間IDを設定
-    price { Faker::Commerce.price(range: 100..10_000) } # ランダムな価格を設定
+    name { 'Sample Item' }
+    description { 'This is a sample description.' }
+    category_id { 2 } # 適当な値に設定
+    status_id { 2 } # 適当な値に設定
+    responsibility_id { 2 } # 適当な値に設定
+    region_id { 2 } # 適当な値に設定
+    time_id { 2 } # Other than '1' (which is '---')
+    price { 500 } # Between 300 and 9,999,999
+    association :user
   end
 end
