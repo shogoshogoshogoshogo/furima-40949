9a6461e6734753f623534574d3c688044a37f691
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-09-25 19:33:21 +0900

    マイグレーション削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_20_112108) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_20_104309) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,11 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_20_112108) do
     t.string "street", null: false
     t.string "building_name"
     t.string "phone_number", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "genres", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
