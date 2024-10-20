52792b6d1ab862f698caa091c05d61f62e8a3649
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-10-20 20:12:48 +0900

    コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+    Rails.logger.debug("Order Params: #{order_params.inspect}")
     if @order_address.valid?
       pay_item
       @order_address.save
