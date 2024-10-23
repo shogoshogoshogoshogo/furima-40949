5a0e08a88f3aed2b52e1a792d3719972596aac37
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-10-23 20:49:15 +0900

    テストコード追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -73,6 +73,24 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is invalid. Only 10 or 11 digit numbers allowed')
       end
+
+      it 'user_idが空では保存できないこと' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空では保存できないこと' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+
+      it 'tokenが空では保存できないこと' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
